7c5afda96522a9e332ba42c6dc7539514f4a1697
Felix Gomez <61999750+TheFelixGomez@users.noreply.github.com>
2026-01-13 18:51:45 -0500

    remove unused go_router import from login_page.dart

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart'; // Import go_router
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'auth_service.dart';
 
